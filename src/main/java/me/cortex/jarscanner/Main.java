b83b6a3a2507a57530700203bac6cbf5f2fa9a0c
solonovamax <solonovamax@12oclockpoint.com>
2023-06-08 00:26:03 -0400

    Cleanup utility classes
    - Make them final
    - Add private constructor

--- src/main/java/me/cortex/jarscanner/Main.java
+++ src/main/java/me/cortex/jarscanner/Main.java
@@ -7,10 +7,13 @@ import picocli.CommandLine;
  *
  * @see RootScannerTask CLI command outline.
  */
-public class Main {
-	public static void main(String[] args) {
-		// Delegate app args to picocli, which will populate the root-scanner parameters and options
-		int exitCode = new CommandLine(new RootScannerTask()).execute(args);
-		System.exit(exitCode);
-	}
+public final class Main {
+    private Main() {
+    }
+
+    public static void main(String[] args) {
+        // Delegate app args to picocli, which will populate the root-scanner parameters and options
+        int exitCode = new CommandLine(new RootScannerTask()).execute(args);
+        System.exit(exitCode);
+    }
 }
